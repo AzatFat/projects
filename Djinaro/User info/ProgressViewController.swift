ba6d67a10134b1232748c59af6048b14af351aea
AzatFat <azat@MBP-Azat-2.Dlink>
2018-12-12 01:16:12 +0300

    Version 1.5.9.5

---
+++ Djinaro/User info/ProgressViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProgressViewController.swift
+//  Djinaro
+//
+//  Created by Azat on 11.12.2018.
+//  Copyright © 2018 Azat. All rights reserved.
+//
+
+import UIKit
+
+class ProgressViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
